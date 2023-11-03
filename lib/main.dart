11dcd523f26b6caea5cef7446f83926dffcb06cc
Zero619 <abanbzakaria3@gmail.com>
2023-11-03 16:35:16 +0000

    asdasd

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: const Text('Flutter Demo Click2 Counter'),
+        title: const Text('Flutter Demo Click2 Counter3'),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
